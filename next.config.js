ebd4ed85069eb1e62093dc615cdf42abc2d5f325
Ntwali <ram@Ntwalis-MacBook-Pro.local>
2025-07-14 15:23:12 +0200

    fixing deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   images: {
-    domains: ["localhost"],
+    domains: ['localhost'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
       },
     ],
   },
